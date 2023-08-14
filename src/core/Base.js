319fd81f438a9e3a43e069d815c3129b1f170e3b
YaswanthSaiCh <ch.yaswanthsai@gmail.com>
2023-08-14 20:08:11 +0530

    feat(): created a base template

---
+++ src/core/Base.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Base = ({
+  title = "Title",
+  description = "Description",
+  classes = "text-center p-4",
+  children,
+}) => {
+  return (
+    <div className="container-fluid p-0">
+      <div data-bs-theme="dark" className="jumbotron text-white text-center">
+        <h2 className="display-4">{title}</h2>
+        <p className="lead">{description}</p>
+      </div>
+      <div>
+        <div data-bs-theme="dark" className={classes}>
+          {children}
+        </div>
+      </div>
+      <footer data-bs-theme="dark" className="footer mt-auto py-3">
+        <div className="container-fluid bg-success text-white text-center py-3">
+          <h4>If you got any questions, feel free to react out...</h4>
+          <button className="btn btn-warning btn-large">Contact us</button>
+        </div>
+        <div className="container">
+          <span className="text-body-secondary">An amazing MERN bootcamp</span>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Base;
---
+++ src/core/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Base from "./Base";
+import "../styles.css";
+
+const Home = () => {
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default Home;
